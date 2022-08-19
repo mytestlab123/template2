cc8a19d189bfa7a5b0aa0112ebefddbb29df18b9
Amit Karpe <amitkarpe@gmail.com>
2022-08-19 08:55:30 +0530

    Initial commit

---
+++ acceptance.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+if [ ! -f ./main ]; then
+    go build -o main .
+fi
+
+./main &
+PROC_ID=$!
+
+curl 'http://localhost:8080/wiki/save/doc'
+curl 'http://localhost:8080/wiki/save/doc'  -H 'Content-Type: application/x-www-form-urlencoded'    --data 'body=hellotestbenchwashere'
+
+acceptedRate=1000
+
+if ! [ -x "$(command -v ab)" ]; then
+    echo installing ab
+    apt-get update
+    apt-get install apache2-utils
+fi
+
+currentRate=$(printf  %.0f `ab -k  -T 'application/x-www-form-urlencoded'    -n 1000 -c 1 'http://localhost:8080/wiki/view/doc' | grep 'Requests per second' | grep -oE [0-9]+.[0-9]+ `)
+
+if [ $currentRate -gt $acceptedRate ]
+then
+    kill -9 $PROC_ID
+    echo Current Rate $currentRate
+    echo PASS
+else
+   kill -9 $PROC_ID
+   echo Acceptance Criteria Failed
+   echo Current Rate $currentRate
+   exit 1
+fi
+
